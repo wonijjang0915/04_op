558cbdbe8090421ccb8bb2212f0daba7173c36f1
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-09-24 16:09:21 +0900

    02

--- main.c
+++ main.c
@@ -4,17 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y, z, m;
-	int a, b, c;
+	int x = 3;
+	int cond1, cond2;
 	
-	x = 1;
-	a = 3;
-	b = 4;
-	c = 5;
+	cond1 = 2 < x && x < 5;
+	cond2 = x > 1 || x < 4 && x > 3;
 	
-	y = a*x*x + b*x + c ;
-	m = (x+y+z) / 3 ;
-	
-	printf("y=%d, m=%d", y, m);
+	printf("cond1=%d, cond2=%d",
+	cond1, cond2);
 	return 0;
 }
